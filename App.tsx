b6bb5333596d35db6d1d77358ee19273864d6a31
quangan010903@gmail.com <quangan010903@gmail.com>
2025-10-30 23:15:28 +0700

    update code

--- App.tsx
+++ App.tsx
@@ -5,63 +5,33 @@ import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from "@expo/vector-icons/Ionicons";
+import HomeDetail from './components/learn/home.detail';
+import Home from './components/learn/home';
+import Like from './components/learn/like';
+
+const TabApp = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Like" component={Like} />
+    </Tab.Navigator>
+  )
+}
+
 export default function App() {
 
   const Stack = createNativeStackNavigator();
-  const Drawer = createDrawerNavigator();
-  const Tab = createBottomTabNavigator();
-
-  const HomeScreen = (props: any) => {
-    const navigation = props.navigation;
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <View style={{ marginVertical: 10 }}>
-          <Button
-            onPress={() => navigation.navigate("Details")}
-            title='Go to Detail' />
-        </View>
-        <View style={{ marginVertical: 10 }}>
-          <Button
-            onPress={() => navigation.navigate("Details",
-              { userId: 1, name: "Khanh An" }
-            )}
-            title='Go user id = 1' />
-        </View>
-        <View style={{ marginVertical: 10 }}>
-          <Button
-            onPress={() => navigation.navigate("Details", {
-              userId: 2, name: "An Dang"
-            })}
-            title='Go user id = 2' />
-        </View>
-
-      </View>
-    );
-  }
-  const DetailsScreen = (props: any) => {
-    const route: any = useRoute();
-    const navigation: any = useNavigation();
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>user id = {route.params.userId}</Text>
-        <Button
-          onPress={() => navigation.goBack()}
-          title='Go back Home' />
-      </View>
-    );
-  }
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
+      <Stack.Navigator>
         <Stack.Screen
           name="Home"
-          component={HomeScreen}
-          options={{ headerTitle: "Trang chủ" }} />
+          component={TabApp}
+          options={{ headerTitle: "Trang chủ", headerShown: false }} />
         <Stack.Screen
           name='Details'
-          component={DetailsScreen}
+          component={HomeDetail}
           options={({ route }: { route: any }) => ({
             headerTitle: `Xem chi tiết ${route?.params?.userId ?? ""}`,
             headerStyle: {
@@ -72,37 +42,7 @@ export default function App() {
               fontWeight: 'bold',
             },
           })} />
-      </Stack.Navigator> */}
-      {/* <Drawer.Navigator initialRouteName='andeptrai'>
-        <Drawer.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{ drawerLabel: "Trang chủ", headerTitle: "Trang chủ" }}></Drawer.Screen>
-        <Drawer.Screen name='Details' component={DetailsScreen}></Drawer.Screen>
-      </Drawer.Navigator> */}
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'american-football'
-                : 'american-football-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'aperture' : 'aperture-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName as any} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Details" component={DetailsScreen} />
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ components/learn/change.password.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const ChangePassword = () => {
+    return (
+        <View>
+            <Text>Change Password Component</Text>
+        </View>
+    )
+}
+export default ChangePassword;
