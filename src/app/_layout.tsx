826a92bddbdb12c60adc21a7181b9e9f9305fe4c
quangan010903@gmail.com <quangan010903@gmail.com>
2025-11-03 21:09:38 +0700

    update code

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,21 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { Text, View } from "react-native";
 
 const RootLayout = () => {
     return (
-        <View style={{padding:50}}>
-            <Text>Header</Text>
-            <Slot/>
-            <Text>Footer</Text>
-        </View>
+        // <View style={{padding:50}}>
+        //     <Text>Header</Text>
+        //     <Slot/>
+        //     <Text>Footer</Text>
+        // </View>
+        <Stack>
+            <Stack.Screen
+                name="(tabs)"
+                options={{ headerTitle: "Trang chủ" }} />
+            <Stack.Screen
+                name="product/index"
+                options={{ headerTitle: "Sản phẩm" }} />
+        </Stack>
     )
 }
 export default RootLayout;
