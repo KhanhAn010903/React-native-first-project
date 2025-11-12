30e59dc14759efb78903c86b822a94c4284544f5
quangan010903@gmail.com <quangan010903@gmail.com>
2025-11-12 20:04:57 +0700

    update code

--- src/utils/validate.schema.ts
+++ src/utils/validate.schema.ts
@@ -1,10 +1,22 @@
 import * as Yup from 'yup';
 export const LoginSchema = Yup.object().shape({
-    password : Yup.string()
+    password: Yup.string()
         .min(2, "Mật khẩu cần tối thiếu 6 ký tự")
-        .max(50,"Mật khẩu tối đa 50 ký tự")
+        .max(50, "Mật khẩu tối đa 50 ký tự")
         .required("Mật khẩu không được để trống"),
-    email : Yup.string()
+    email: Yup.string()
         .email("Định dạng email không hợp lệ")
         .required("Email không được để trống"),
 });
+
+export const SignUpSchema = Yup.object().shape({
+    password: Yup.string()
+        .min(6, "Mật khẩu cần tối thiếu 6 ký tự")
+        .max(50, "Mật khẩu tối đa 50 ký tự")
+        .required("Mật khẩu không được để trống"),
+    email: Yup.string()
+        .email("Định dạng email không hợp lệ")
+        .required("Email không được để trống"),
+    name: Yup.string()
+        .required("Họ và tên không được để trống"),
+})
